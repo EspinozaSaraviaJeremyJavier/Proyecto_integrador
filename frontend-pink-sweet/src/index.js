1b82b07789dcfe9135eaacc584bae803c1e9d6ed
CloudLSR <bernardbombsquad@gmail.com>
2026-06-04 02:30:05 -0500

    Feat: Añade Página del Catálogo de Productos
    http://localhost:3000/productos para poder acceder directamente a la página

--- frontend-pink-sweet/src/index.js
+++ frontend-pink-sweet/src/index.js
@@ -5,16 +5,20 @@ import App from './App';
 import Login from './Login';
 import Registro from './Registro';
 import Perfil from './Perfil';
-import '/index.css';
+// Productos
+import Productos from './Productos';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/"         element={<App />} />
-      <Route path="/login"    element={<Login />} />
-      <Route path="/registro" element={<Registro />} />
-      <Route path="/perfil" element={<Perfil />} />
+      <Route path="/"          element={<App />} />
+      <Route path="/login"     element={<Login />} />
+      <Route path="/registro"  element={<Registro />} />
+      <Route path="/perfil"    element={<Perfil />} />
+      <Route path="/productos" element={<Productos />} />
     </Routes>
   </BrowserRouter>
 );
